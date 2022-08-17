85b0fe56e66eb739e2d48573ac83af7bfe3e62cc
Daniel <goodchild1983@gmail.com>
2022-08-16 23:00:44 -0700

    just in case

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,4 @@
-// const { transparent } = require('tailwindcss/colors')
-// const color = require('tailwindcss/color')
+const colors = require('tailwindcss/colors')
 
 module.exports = {
   content: [
@@ -9,9 +8,27 @@ module.exports = {
     './content/**/*.md'
   ],
   theme: {
-    extend: {},
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+    },
+    extend: {
+      colors: {
+        burgundy: '#2d2d2d',
+        darkBrown: '#634321',
+        lightBrown: '#A78C51',
+        lightTan: '#f5f5f5',
+        veryLightTan: '#f5f5f5',
+        darkGreen: '#264027',
+        apricot: '#f5f5f5',
+
+    },
   },
   plugins: [
     require('@tailwindcss/typography'),
   ],
+},
 }
+
